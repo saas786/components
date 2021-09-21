ec76565370e120789e1b7a065094f79889a20b21
Brian Dillingham <brian@dillingham.dev>
2021-09-21 19:26:36 -0400

    wip

--- src/Elements/Detail.vue
+++ src/Elements/Detail.vue
@@ -2,12 +2,18 @@
   <div class="border-gray-200 px-4 py-5 sm:p-0">
     <dl class="sm:divide-y sm:divide-gray-200">
       <div v-for="key in Object.keys(data)" class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="capitalize text-sm font-medium text-gray-500">
-          {{  key.replaceAll('_', ' ')  }}
-        </dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          {{  data[key]  }}
-        </dd>
+        <slot :name="`row.${key}`" :value="data[key]">
+          <dt class="capitalize text-sm font-medium text-gray-500">
+            <slot :name="`key.${key}`" :value="data[key]">
+              {{  key.replaceAll('_', ' ')  }}
+            </slot>
+          </dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <slot :name="`value.${key}`" :value="data[key]">
+              {{  data[key]  }}
+            </slot>
+          </dd>
+        </slot>
       </div>
     </dl>
   </div>
