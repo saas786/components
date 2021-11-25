7551e1ec638955d9fb4360f62bfdf2ce6ba2198d
Brian Dillingham <brian@dillingham.dev>
2021-11-24 22:05:07 -0500

    Add initial form test

--- src/Elements/Form.cy.jsx
+++ src/Elements/Form.cy.jsx
@@ -1,11 +1,16 @@
 import Form from './Form.vue'
 
 describe('Form', () => {
+
     it('renders', () => {
-        // cy.mount(<Form />, {
-        //     props: {
-        //         fields: ['title']
-        //     }
-        // })
+        cy.mount(<Form />, {
+            props: {
+                action: '/users/new',
+                fields: ['title', 'description']
+            }
+        })
+
+        cy.get('form input').should('have.length', 2)
     })
+
 })
