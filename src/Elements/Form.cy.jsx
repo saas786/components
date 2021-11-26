f291a513bc73d9393106f793aa62a194d3c945d0
Brian Dillingham <brian@dillingham.dev>
2021-11-26 12:37:15 -0500

    Add more form test cases

--- src/Elements/Form.cy.jsx
+++ src/Elements/Form.cy.jsx
@@ -1,6 +1,6 @@
 import Form from './Form.vue'
 
-describe('Form', () => {
+describe('Form Setup', () => {
 
     it('renders form element with attributes', () => {
         cy.mount(<Form />, {
@@ -18,8 +18,11 @@ describe('Form', () => {
             .invoke('attr', 'method')
             .should('eq', 'POST')
     })
+})
 
-    it('renders fields when passed a flat array of field names', () => {
+describe('Form Fields', () => {
+
+    it('with a flat array of field names', () => {
         cy.mount(<Form />, {
             props: {
                 action: '/users/new',
@@ -34,7 +37,7 @@ describe('Form', () => {
         cy.get('input[name="description"]').should('exist').should('not.have.value')
     })
 
-    it('renders fields with values when passed a values object', () => {
+    it('with values when passed a values object', () => {
         cy.mount(<Form />, {
             props: {
                 fields: ['title', 'description'],
@@ -46,7 +49,7 @@ describe('Form', () => {
         cy.get('input[name="description"]').should('have.value', 'row row row your boat')
     })
 
-    it('renders fields when only passed a values object', () => {
+    it('with a values object only', () => {
         cy.mount(<Form />, {
             props: {
                 values: {title: 'Titanic', description: 'row row row your boat'}
@@ -57,7 +60,7 @@ describe('Form', () => {
         cy.get('input[name="description"]').should('have.value', 'row row row your boat')
     })
 
-    it('renders fields when only passed a values object and excludes', () => {
+    it('with a values object and excludes', () => {
         cy.mount(<Form />, {
             props: {
                 action: '/users/new',
@@ -70,7 +73,7 @@ describe('Form', () => {
         cy.get('input[name="description"]').should('not.exist')
     })
 
-    it('renders fields from an object', () => {
+    it('with an object', () => {
         cy.mount(<Form />, {
             props: {
                 fields: [
@@ -86,7 +89,7 @@ describe('Form', () => {
         cy.get('input[name="description"]').should('exist')
     })
 
-    it('renders field object default value', () => {
+    it('with an object & default value', () => {
         cy.mount(<Form />, {
             props: {
                 fields: [
@@ -100,24 +103,47 @@ describe('Form', () => {
         cy.get('input[name="description"]').should('not.have.value')
     })
 
-    it.skip('renders field separators', () => {
+    it('with an object & width span', () => {
+        cy.mount(<Form />, {
+            props: {
+                fields: [
+                    { name: 'title', span: 6 },
+                    { name: 'description', span: 3 },
+                    { name: 'author_id' }
+                ]
+            }
+        })
 
-        // logic error (not a testing problem).
+        cy.get('[data-testid="field-title"]').should('have.class', 'col-span-6')
+        cy.get('[data-testid="field-description"]').should('have.class', 'col-span-3')
+        cy.get('[data-testid="field-author_id"]').should('have.class', 'col-span-12')
+    })
+})
+
+describe('Form Field Extras', () => {
+
+    it('with a divider', () => {
 
         cy.mount(<Form />, {
             props: {
                 fields: [
-                    { name: 'title', value: 'Titanic' },
+                    { name: 'title' },
                     { divider: true },
                     { name: 'description' },
                 ]
             }
         })
+
+        cy.get('[data-testid="field-title"]')
+            .siblings('[data-testid="field-divider"]')
+            .should('have.length', 1)
+
+        cy.get('[data-testid="field-divider"]')
+            .siblings('[data-testid="field-description"]')
+            .should('have.length', 1)
     })
 
-    it.skip('renders section titles between fields', () => {
-
-        // logic error (not a testing problem).
+    it('with a section titles', () => {
 
         cy.mount(<Form />, {
             props: {
@@ -129,35 +155,71 @@ describe('Form', () => {
             }
         })
 
-        cy.get('div[data-testid="section_title"]')
+        cy.get('[data-testid="field-title"]')
+            .siblings('[data-testid="field-section_title"]')
             .should('have.length', 1)
+            .should('have.text', 'Some title')
 
-        cy.get('div[data-testid="field-title"]')
-            .next('div[data-testid="section_title"]')
-            .should('have.text', 'Some Title')
-
-        cy.get('div[data-testid="section_title"]')
-            .next('div[data-testid="field-description"]')
+        cy.get('[data-testid="field-section_title"]')
+            .siblings('[data-testid="field-description"]')
             .should('have.length', 1)
     })
+})
 
-    it.skip('renders field error messages', () => {
-        cy.mount(<Form />, {
-            props: {
-                fields: ['title']
-            },
-            data() {
-                return {
-                    errors: {
-                        title: 'The title field is required.'
-                    }
-                }
-            },
-        })
+describe('Form Field Slots', () => {
+    it.skip('overrides entire field block via slot', () => {
 
-        cy.get('div[data-testid="error"]').should('have.length', 1)
-        cy.get('div[data-testid="error"]').first().should('have.text', 'The title field is required.')
+        // period in slot name is breaking: unknown: Unexpected token
+        // also can we use slot shorthand? #field.title.all="" ?
+
+        // cy.mount(() => (
+        //     <Form>
+        //         <template v-slot:field.title.all="{ form }">
+        //             <span id="test_output" v-text="form.title" />
+        //             <input name="title" v-model="form.title" />
+        //         </template>
+        //     </Form>
+        // ), {
+        //     props: {
+        //         fields: ['title', 'description']
+        //     }
+        // });
+
+        cy.get('form input').should('have.length', 2)
+        cy.get('label[for="title"]').should('not.exist')
+        cy.get('label[for="description"]').should('contain.text', 'Description')
+        cy.get('input[name="title"]').should('exist').should('not.have.value')
+        cy.get('input[name="description"]').should('exist').should('not.have.value')
+        cy.get('input[name="title"]').type('hello world')
+        cy.get('span#test_output').should('have.text', 'hello world')
     })
+
+    it.skip('overrides a field block input and keep label & error', () => {
+
+        // period in slot name is breaking: unknown: Unexpected token
+
+        // cy.mount(() => (
+        //     <Form>
+        //         <template v-slot:field.title="{ form }">
+        //             <span id="test_output" v-text="form.title" />
+        //             <input name="title" v-model="form.title" />
+        //         </template>
+        //     </Form>
+        // ), {
+        //     props: {
+        //         fields: ['title', 'description']
+        //     }
+        // });
+
+        cy.get('form input').should('have.length', 2)
+        cy.get('label[for="title"]').should('contain.text', 'Title')
+        cy.get('label[for="description"]').should('contain.text', 'Description')
+        cy.get('input[name="title"]').type('hello world')
+        cy.get('span#test_output').should('have.text', 'hello world')
+    })
+})
+
+describe('Form Buttons', () => {
 
     it('submit button is "create" by default', () => {
         cy.mount(<Form />)
@@ -199,5 +261,42 @@ describe('Form', () => {
 
         cy.get('button[data-testid="cancel"]').should('exist')
     })
+})
 
+describe('Form Submit', () => {
+    it.skip('renders success message at the top', () => {})
+    it.skip('renders error message at the top', () => {})
+    it.skip('renders single error message for field', () => {
+        cy.intercept('POST', '/users/new', {
+            fixture: 'inertia-error-new-user',
+            statusCode: 422,
+            headers: {
+                'Vary': 'Accept',
+                'X-Inertia': 'true',
+            }
+        }).as('formSubmit')
+
+        cy.mount(<Form />, {
+            props: {
+                action: '/users/new',
+                fields: ['title', 'description'],
+                formHandler: {
+                    processing: false,
+                    submit() {
+                      //
+                    },
+                    reset() {
+                        //
+                    }
+                }
+            }
+        })
+
+        cy.get('button[type="submit"]').click()
+
+        cy.wait('@formSubmit').then((interception) => {
+            cy.get('div[data-testid="error"]').should('have.length', 1)
+            cy.get('div[data-testid="error"]').first().should('have.text', 'The title field is required.')
+        })
+    })
 })
